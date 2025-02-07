099d7e80f0211701687e1edb3a6eb0cf8ddbd833
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-07 00:00:32 -0500

    improve clustering display

--- public/js/app.js
+++ public/js/app.js
@@ -49,21 +49,16 @@ function createOverviewMap(dataset) {
       source: "portals",
       filter: ["has", "point_count"],
       paint: {
-        // Use step expressions (https://maplibre.org/maplibre-style-spec/#expressions-step)
-        // with three steps to implement three types of circles:
-        //   * Blue, 20px circles when point count is less than 100
-        //   * Yellow, 30px circles when point count is between 100 and 750
-        //   * Pink, 40px circles when point count is greater than or equal to 750
         "circle-color": [
           "step",
           ["get", "point_count"],
           "#51bbd6",
-          100,
+          10,
           "#f1f075",
-          750,
+          100,
           "#f28cb1",
         ],
-        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+        "circle-radius": 20,
       },
     });
 
