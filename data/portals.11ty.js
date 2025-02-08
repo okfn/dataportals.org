cd5f9d19d3cb1f318a01ec092853aa868a89e02f
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-08 00:49:23 -0500

    centralize data loading

--- data/portals.11ty.js
+++ data/portals.11ty.js
@@ -1,12 +1,4 @@
 const model = require("../lib/model.js");
-const csvparse = require("csv-parse/sync");
-const fs = require("node:fs");
-const path = require("node:path");
-
-const csvPath = path.join(__dirname, "/portals.csv");
-const csvData = fs.readFileSync(csvPath);
-const jsonData = csvparse.parse(csvData, { columns: true });
-model.catalog.load(jsonData);
 
 // https://www.11ty.dev/docs/languages/javascript/#classes
 class PortalsData {
